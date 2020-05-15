8b20773a89b1fad85014ad88d22a4465b65d8d46
sajogergo <sajo.gergo@gmail.com>
2020-05-15 16:49:01 +0200

    changed

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function() {
+btn.addEventListener("click", function() {
     let hexColor = "#";
 
-    for ( let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()]
     }
 
